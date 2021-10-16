bbab216bb71638452e4029ef116a338ca849eed4
Victor Hugo <victorhugofny@gmail.com>
2021-10-15 22:00:02 -0300

    feat estrutura inicial vue

---
+++ calculator/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+      <h1> Ola Vue! </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono" ;
+    src: url("./fonts/RobotoMono-VariableFont_wght.ttf");
+}
+
+*{
+    font-family: "RobotoMono", monospace;
+}
+
+body{
+    margin: 0
+}
+
+#app{
+    display:flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: rgb(255, 255, 255);
+    background: black;
+    
+}
+</style>
